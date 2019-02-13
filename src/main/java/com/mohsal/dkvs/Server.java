050483fbbe72593af326e8c5e611a538c398ccd5
Mohamed Saleem <mohsal.saleem@gmail.com>
2019-02-14 01:49:57 +0530

    Code cleanup

--- src/main/java/com/mohsal/dkvs/Server.java
+++ src/main/java/com/mohsal/dkvs/Server.java
@@ -16,24 +16,38 @@ public class Server {
     private static final Logger LOGGER =
             Logger.getLogger(Server.class.getName());
 
+    /**
+     * Hostname
+     */
     private String host;
 
+    /**
+     * Port in which the server will run on.
+     */
     private int port;
 
-    // Map of other servers in the cluster. ServerID - Host, Port
+    /**
+     * Map of other servers in the cluster. ServerID - Host, Port
+     */
     private ConcurrentHashMap<Integer, Pair<String, Integer>> serverMap;
 
-    // Map of the key value store. Key - Clock, Value
-    // Clock represents the vector clock to identify the values newness.
+    /**
+     * Map of the key value store. Key - Clock, Value
+     * Clock represents the vector clock to identify the values newness.
+     */
     private ConcurrentHashMap<String, Pair<Long, String>> store;
 
-    // Map of the key value store. Key - Clock, Value
-    // Clock represents the vector clock to identify the values newness.
-    // This map contains the keys and values that have not been propagated between the server nodes yet.
+    /**
+     * Map of the key value store. Key - Clock, Value
+     * Clock represents the vector clock to identify the values newness.
+     * This map contains the keys and values that have not been propagated between the server nodes yet.
+     */
     private ConcurrentHashMap<String, Pair<Long, String>> writeAheadLog;
 
-    // TODO: Replace the ServerID with an Object that encapsulates server information such as ServerID, type of node - Client or Server or Registry
-    // Maintain a map of connections to the servers in the cluster. ServerID - Socket
+    /**
+     * TODO: Replace the ServerID with an Object that encapsulates server information such as ServerID, type of node - Client or Server or Registry
+     * Maintain a map of connections to the servers in the cluster. ServerID - Socket
+     */
     private ConcurrentHashMap<Long, Socket> serverConnections;
 
     Server(String host, int port) {
@@ -45,9 +59,11 @@ public class Server {
         this.serverConnections = new ConcurrentHashMap<>();
     }
 
-    // Register myself in a service registry
-    // Retrieve all other server information in the cluster and save them
-    // Setup a thread that periodically refreshes the cluster's servers data by fetching them from the registry.
+    /**
+     * Setup a thread that periodically refreshes the cluster's servers data by fetching them from the registry.
+     * Retrieve all other server information in the cluster and save them
+     * Register myself in a service registry
+     */
     public void initializeServer() {
 
     }
